cb60638c355dc8c09b962cfd3983bc26fc3a0117
SeungRhee Oh <sroh@SR5HMBA.local>
2021-07-14 01:27:19 +0900

    Applied set-difference for removing duplicated suit-numbers in hand.

--- src/poker.lisp
+++ src/poker.lisp
@@ -3,24 +3,28 @@
 (in-package :poker)
 
 (defconstant +suits+ '(spade diamond heart clover))
-(defconstant +number-each-suit+ 13)
 (defconstant +numbers+ '(1 2 3 4 5 6 7 8 9 10 J Q K))
-;;((s1 s2 s3 s4 s5 s6 s7 s8 s9 s10 sj sq sk) (c1  c2 c3 c4 c5 c6 c7 c8 c9 c10 cj cq ck) (d1 d2 d3 d4 d5 d6 d7 d8 d9 d10 dj dq dk) (h1 h2 h3 h4 h5 h6 h7 h8 h9 h10 hj hq hk))
-;; ((1 2 .. K)..5)
-(defun generate (remained-cards &optional (generated nil))
+(defun generate (remained-cards &optional (generated nil) (permutation nil))
   (cond ((eq remained-cards nil) generated)
 	((eq generated nil)
 	 (generate (cdr remained-cards)
 		   (mapcar #'(lambda (r)
 			       (list r))
-			   (car remained-cards))))
+			   (car remained-cards))
+		   permutation))
 	(t
 	 (generate (cdr remained-cards)
-		   (mapcan #'(lambda (r)
-			       (mapcar #'(lambda (g)
+		   (mapcan #'(lambda (g)
+			       (mapcar #'(lambda (r)
 					   (cons r g))
-				       generated))
-			   (car remained-cards))))))
+
+				       (if permutation
+					   (set-difference (car remained-cards) g :test #'equal)
+					   (car remained-cards))
+				       
+				       ))
+			   generated)
+		   permutation))))
 
 (defun append-number (combination-suits numbers &optional (generated nil))
   (cond ((eq combination-suits nil) generated)
@@ -35,36 +39,26 @@
 			      generated)))))
 
 
-(defun generate-combinations ()
-  (let ((combi-suits (generate '((c d h s)
-				 (c d h s)
-				 (c d h s)
-				 (c d h s)
-				 (c d h s)))))
-    (let ((suits (append-number combi-suits +numbers+)))
-      (labels ((inner (s &optional (acc nil))
-		 (cond ((null s)
-			acc)
-		       (t
-			(inner (cdr s) (cons (generate (car s)) acc))))))
-	(inner suits)))))
-
 (defun test ()
-  (with-open-file (fs "./test.txt"
-		      :direction :output
-		      :if-exists :supersede
-		      :if-does-not-exist :create)
-    (let ((combi-suits (generate '((c d h s)
-				 (c d h s)
-				 (c d h s)
-				 (c d h s)
-				 (c d h s)))))
-      (let ((suits (append-number combi-suits +numbers+)))
-	(labels ((inner (s &optional (acc nil))
-		   (cond ((null s)
-			  acc)
+  
+  (let ((combi-suits (generate '((c d h s)
+				  (c d h s)
+				  (c d h s)
+				  (c d h s)
+				  (c d h s)))))
+    (let ((suits (append-number combi-suits +numbers+)))
+      (labels ((inner (s &optional (n 1))
+		 (let ((fs (open (concatenate 'string "./test" (format nil "~a" n) ".txt")
+				 :direction :output
+				 :if-exists :supersede
+				 :if-does-not-exist :create)))
+		   (cond ((null s) nil)
 			 (t
-			  (inner (cdr s) (cons (generate (car s)) acc))))))
-	  (let ((s (inner suits)))
-	    (dolist (l s)
-	      (format fs "~a~%" l))))))))
+			  (print (car s))
+			  (let ((s1 (generate (car s) nil t)))
+			    (dolist (l s1)
+			      (format fs "~{~a ~}~%" l))
+			    (close fs))
+			  (inner (cdr s) (+ n 1)))))))
+	(inner suits)
+	'DONE))))
