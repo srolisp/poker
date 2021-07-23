e94d342aaff7fd3420cb620ca936ad82fc693abb
SeungRhee Oh <sroh@SR5HMBA.local>
2021-07-23 18:20:14 +0900

    In the chaos.

--- src/poker.lisp
+++ src/poker.lisp
@@ -6,8 +6,8 @@
 (defconstant +numbers+ '(01 02 03 04 05 06 07 08 09 10 11 12 13))
 (defconstant +number-in-hand+ 5)
 
-;; states: ((C) (D) (H) (S) (C C) ....)
-;;         ((C C C C C) ....(C C C) ... (D) (H) (S))
+;; states: ((S) (D) (H) (C))
+;;         ((C C C C C) ....(C C C) ... (D) (H) (C))
 ;;         ...
 ;;
 ;; nil means that all cards searched.
@@ -16,55 +16,82 @@
 		   (goal-p)
 		   (selector)
 		   (successor)
-		   (merge) 	; like DFS
+		   (merge)     
 		   (post-proc)
-		   ;; (args '((acc)))
 		   (args))
   (cond ((funcall goal-p states) args)
 	(t (let ((selected (funcall selector states)))
-	     ;; (if (eq post-proc #'pproc2) (print states))
 	     (generate (funcall merge
-				(funcall successor selected)
+				(funcall successor selected args)
 				(cdr states))
 		       goal-p selector successor merge post-proc
 		       (funcall post-proc selected args) ; must return args
 		       )))))
 	   	   
-;; ((eq suits 'start) (mapcar #'(lambda (s) (list s)) +suits+))
+(defun check-already-made (seq table)
+  (funcall (funcall table 'lookup) seq))
+
 ;; successor function
 ;; (C S ...)
-;; (defun in-hands (suits &optional (possible +suits+))
-;;   (cond ((= +number-in-hand+ (length suits)) nil)
-;; 	(t (mapcar #'(lambda (s) (cons s suits)) possible))))
 (defun in-hands (possible)
-  (lambda (suits)
-    (cond ((= +number-in-hand+ (length suits)) nil)
+  (lambda (suits args)
+    (cond ((= 6 (length suits)) ;TODO: 6 (f C01 C02 ..) with tag f.
+	   nil)			; no successor: expanding is over.
+	  ((= +number-in-hand+ (length suits))
+	   (let ((freq (get-frequency-cards suits)))
+	     (cond ((check-already-made  freq (car args))
+		    nil)
+		   ;; suits : (C C C C C)
+		   ;; new-possible : (C01 C02 C03 C04 C05)
+		   (t (funcall (funcall (car args) 'insert) freq suits)
+		      (let ((new-possible (_append-number suits +numbers+))
+			    (hands (if (is-flush freq) '((flush)) nil))) ; ((C01 C02 ...)
+					;  (C01 
+					;  ..
+					;
+					;  (C01 .....  ))
+			
+			;; generated: args
+			;; TODO: when (C C C C C), get all successor list ((c01 c03 c05 c06 c08) ..)
+			(let ((generated (generate (funcall (in-hands (car new-possible)) nil nil)
+						   #'null
+						   #'car
+						   (temp new-possible)
+						   #'append
+						   #'pproc
+						   (append (append args (list suits)) hands))))
+			  ;; r: ((C01 C02 C03 ..) ...(C10..C13))
+			  (let ((r (funcall (funcall (caddr generated) 'lookup) suits)))
+			    (mapcar #'(lambda (r1)
+					(cons 'f r1))
+				    r)))))))
+	   )
 	  (t (mapcar #'(lambda (s) (cons s suits)) possible)))))
 
-
-
+(defun is-flush (freq)
+  (find-if #'(lambda (x) (= x 5)) freq))
 
 ;; in-hands: (C S..)
-;; args: (..)
-;; return : args
-(defun pproc (in-hands args)
-  (cond ((= +number-in-hand+ (length in-hands))
-	 (let ((freq (analyze-cards in-hands)))
-	   (if (not (funcall (funcall (cadr args) 'lookup) freq))
-	       (funcall (funcall (car args) 'insert) '(acc) in-hands))
-	   (funcall (funcall (cadr args) 'insert) freq in-hands))
-	 args)
-	(t  args)))
+;; in-hands: (f C01 D02 ..)
 
 ;; return: (1 2 1 ..)
-(defun analyze-cards (in-hands &optional (freq '((S . 0) (D . 0) (H . 0) (C . 0))))
+(defun get-frequency-cards (in-hands &optional (freq '((S . 0) (D . 0) (H . 0) (C . 0))))
   (cond ((null in-hands) (mapcar #'(lambda (kv) (cdr kv)) freq))
-	(t (analyze-cards (cdr in-hands)
+	(t (get-frequency-cards (cdr in-hands)
 			  (mapcar #'(lambda (kv)
 				      (if (eq (car kv) (car in-hands))
 					  (cons (car kv) (+ 1 (cdr kv)))
 					  kv))
 				  freq)))))
+;; (C C C C C)
+
+(defun _append-number (suits numbers)
+  (mapcar #'(lambda (c) ; c
+	      (mapcar #'(lambda (n)
+			  ;; (intern (format nil "~a~a" c n)))
+			  (intern (format nil "~a~2,'0D" c n)))
+		      numbers))
+	  suits))
 
 (defun append-number (combination-suits numbers &optional (generated nil))
   (cond ((eq combination-suits nil) generated)
@@ -80,79 +107,120 @@
 			      generated)))))
 
 ;; sorted suits
-(defun sort-suits (suits)
+;; built-in function sort doesn't maintain original input.
+(defun sort-suits (suits &key (key #'identity) (test #'string-lessp))
   (labels ((inner (new sorted &optional passed)
 	     (cond ((null sorted) (append passed (list new)))
-		   ((string-lessp new (car sorted)) (append passed (cons new sorted)))
+		   ((funcall test (funcall key new) (funcall key (car sorted))) (append passed (cons new sorted)))
 		   (t (inner new (cdr sorted) (append passed (cons (car sorted) nil)))))))
     (inner (car suits) (cdr suits))))
 
+;; sorted suits (S10 C05 C03 H03 D01)
+(defun pattern-number (func sorted-suits &optional acc (acc2 1) (max (car sorted-suits)))
+  (labels ((inner (item remained)
+	     (cond ((null item) (pattern-number func nil acc acc2 max))
+		   (t (let ((p (funcall func item (car remained))))
+			(cond (p (pattern-number func (cons item (cdr remained)) acc (+ acc2 1) max))
+			      (t (pattern-number func remained (append acc (list acc2)) 1 max))))))))
+    (cond ((null sorted-suits) (values acc (parse-integer (subseq (symbol-name max) 1))))
+	  (t (inner (car sorted-suits) (cdr sorted-suits))))))
+
+;; possible is not sorted.
 (defun temp (possible)
   (labels ((succ (n p)
 	     (cond ((= n 0) (car p))
 		   (t (succ (- n 1) (cdr p))))))
-    (lambda (suits)
+    (lambda (suits args)
+      (declare (ignore args))
       (let ((num-in-hands (length suits)))
-	(cond ((= +number-in-hand+ num-in-hands) nil)
-	      (t (let ((p (succ num-in-hands possible)))
-		   ;; (print suits)
-		   ;; (print (set-difference p suits))
-		   (let ((r (mapcar #'(lambda (p1) ;; (cons p1 suits)
-					(sort-suits (cons p1 suits))
-					)
-				    (set-difference p suits))))
+	(let ((p (succ num-in-hands possible)))
+	  (cond ((= +number-in-hand+ num-in-hands) ; 5
+		 nil)
+		(t 
+		 (let ((r (mapcar #'(lambda (p1) ;; (cons p1 suits)
+				      ;; sorted suits (D01 C03 H03 C05 S10)
+				      (sort-suits (cons p1 suits)
+						  :key #'(lambda (s) (subseq (symbol-name s) 1))
+						  :test #'string-greaterp))
+				  (set-difference p suits))))
+		   r))))))))
 
-		     r))))))))
+;; (defun temp (possible)
+;;   (labels ((succ (n p)
+;; 	     (cond ((= n 0) (car p))
+;; 		   (t (succ (- n 1) (cdr p))))))
+;;     (lambda (suits args)
+;;       (let ((num-in-hands (length suits)))
+;; 	(let ((p (succ num-in-hands possible)))
+;; 	  (cond ((= num-in-hands (- +number-in-hand+ 1)) ; 4
+;; 		 (let ((sorted nil)
+;; 		       (successors nil))
+;; 		   (mapc #'(lambda (p1)
+;; 			     (setf sorted (sort-suits (cons p1 suits)
+;; 						  :key #'(lambda (s) (subseq (symbol-name s) 1))
+;; 						  :test #'string-greaterp))
+;; 			     (cond ((check-already-made sorted (cadr args))
+;; 				    nil)
+;; 				   (t
+;; 				    (funcall (funcall (cadr args) 'insert) sorted
+;; 					     ;; (pattern-number #'(lambda (s1 s2)
+;; 					     ;; 			 (string= (subseq (symbol-name s1) 1)
+;; 					     ;; 				      (subseq (symbol-name s2) 1)))
+;; 					     ;; 		     sorted)
+;; 					     sorted)
+;; 				    ;; (funcall (funcall (caddr args) 'insert) (cadddr args) sorted)
+;; 				    (setf successors (cons sorted successors)))))
+;; 			 (set-difference p suits))
+;; 		   successors))
+;; 		((= +number-in-hand+ num-in-hands) ; 5
+;; 		 nil)
+;; 		(t 
+;; 		 (let ((r (mapcar #'(lambda (p1) ;; (cons p1 suits)
+;; 				      ;; sorted suits (D01 C03 H03 C05 S10)
+;; 				      (sort-suits (cons p1 suits)
+;; 						  :key #'(lambda (s) (subseq (symbol-name s) 1))
+;; 						  :test #'string-greaterp))
+;; 				  (set-difference p suits))))
+;; 		   r))))))))
 
-(defun pproc2 (in-hands args)
+(defun pproc (in-hands args)
+  (declare (ignore in-hands))
+  args)
+
+;; sorted (C01 C02 ..)
+(defun pproc2 ()
+  (let ((fs (open (concatenate 'string "./test" (format nil "~a" n) ".txt")
+		  :direction :output
+		  :if-exists :supersede
+		  :if-does-not-exist :create))
+	(n 1))
+    (lambda (in-hands args)
+      (cond ((= +number-in-hand+ (length in-hands))
+	     (format fs "~{~a ~}~%" in-hands)
+	     (setf n (+ n 1)))
+	    (t ))
+      
+      args)))
+
+(defun pproc-with-tag (in-hands args)
   (cond ((= +number-in-hand+ (length in-hands))
-	 (if (not (funcall (funcall (cadr args) 'lookup) in-hands))
-	     (funcall (funcall (car args) 'insert) '(acc) in-hands))
-	 (funcall (funcall (cadr args) 'insert) in-hands in-hands)
+	 (if (not (funcall (funcall (cadr args) 'lookup) (cons 'f in-hands)))
+	     (funcall (funcall (car args) 'insert) '(acc) (cons 'f in-hands)))
+	 (funcall (funcall (cadr args) 'insert) (cons 'f in-hands) (cons 'f in-hands))
 	   args)
 	 (t args)))
 
 (defun test ()
-  (let ((combi-suits (cdr (assoc 'acc (funcall
-				       (funcall (car (generate '((s) (d) (h) (c))
-							       #'null
-							       #'car
-							       (in-hands +suits+)
-							       #'append
-							       #'pproc
-							       (list
-								(make-table :name 'accs)
-								(make-table :name 'freq))))
-						'records))))))
-    (let ((suits (append-number combi-suits +numbers+)))
-      (labels ((inner (s &optional (n 1) (cumul 0))
-		 ;; (let ((fs (open (concatenate 'string "./test" (format nil "~a" n) ".txt")
-		 ;; 		 :direction :output
-		 ;; 		 :if-exists :supersede
-		 ;; 		 :if-does-not-exist :create)))
-		   (cond ((null s) cumul)
-			 (t
-			  (let ((temp-suits (car s))
-				(cum 0))
-			    ;; (print temp-suits)
-			    (let ((cards-at-first (funcall (in-hands (car temp-suits)) nil)))
-			      ;; (print cards-at-first)
-			      (let ((generated (generate cards-at-first
-							    #'null
-							    #'car
-							    (temp temp-suits)
-							    #'append #'pproc2
-							    (list
-							     (make-table :name 'accs)
-							     (make-table :name 'freq
-									 :proc #'(lambda (v rv)
-										   (declare (ignore rv))
-										   v))))))
-				(let ((r (cdr (assoc 'acc (funcall (funcall (car generated) 'records))))))
-				  (dolist (x r)
-				    (setf cum (+ (length x) cum)))
-				;;   (format fs "~{~a ~}~%" x))
-				;; (close fs)
-				)))
-			  (inner (cdr s) (+ n 1) (+ cumul cum)))))))
-	(inner suits)))))
+  (generate '((s) (d) (h) (c))
+	    #'null
+	    #'car
+	    (in-hands +suits+)
+	    #'append
+	    #'pproc
+	    (list
+	     (make-table :name 'freq)
+	     (make-table :name 'accs)
+	     (make-table :name 'accs2)
+	     ))
+  )
+
